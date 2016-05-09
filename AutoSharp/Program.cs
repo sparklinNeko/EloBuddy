4b88be9916cb6091b06e1bf98e848ad4aa752af3
sparklinNeko <holger.svenson@gmx.de>
2016-05-10 00:41:35 +0200

    Update Program.cs

--- AutoSharp/Program.cs
+++ AutoSharp/Program.cs
@@ -14,7 +14,6 @@ using SharpDX;
 using Color = System.Drawing.Color;
 using AutoSharp.Properties;
 using PortAIO.Utility;
-using LeagueSharp.Common;
 // ReSharper disable ObjectCreationAsStatement
 
 namespace AutoSharp
